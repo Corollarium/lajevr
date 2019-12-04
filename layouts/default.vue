094abb045006ba8baa7ab6274fecc8c4ca3772c8
Bruno <brunobg@gmail.com>
2019-12-04 16:56:59 -0300

    layouts and middleware

---
+++ layouts/default.vue
@@ -0,0 +1,49 @@
+<template>
+  <div>
+    <SiteHeader />
+    <nuxt />
+    <SiteFooter />
+  </div>
+</template>
+
+<script>
+import SiteHeader from '~/components/SiteHeader.vue';
+import SiteFooter from '~/components/SiteFooter.vue';
+
+export default {
+  components: {
+    SiteHeader,
+    SiteFooter
+  }
+};
+</script>
+
+<style lang="less">
+html {
+  word-spacing: 1px;
+  -ms-text-size-adjust: 100%;
+  -webkit-text-size-adjust: 100%;
+  -moz-osx-font-smoothing: grayscale;
+  -webkit-font-smoothing: antialiased;
+  box-sizing: border-box;
+}
+
+html, body {
+  margin:0;
+  padding:0;
+  height:100%;
+}
+
+*,
+*:before,
+*:after {
+  box-sizing: border-box;
+  margin: 0;
+}
+
+figcaption {
+  font-size: 0.8em;
+  font-style: italic;
+}
+
+</style>
