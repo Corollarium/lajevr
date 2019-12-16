eb95e4714ac72595c9b37a32110d8924b7d46c36
Bruno <brunobg@gmail.com>
2019-12-16 14:34:19 -0300

    pages/underwater: create

---
+++ pages/underwater.vue
@@ -0,0 +1,24 @@
+<template>
+  <article>
+    <section>
+      <Underwater />
+    </section>
+  </article>
+</template>
+
+<script>
+import Underwater from '~/components/Underwaterb.vue';
+
+export default {
+
+  components: {
+    Underwater
+  },
+  data () {
+    return {
+    };
+  }
+};
+</script>
+
+<style></style>
