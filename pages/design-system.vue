a18e0a05717143fe765cf1d01b95f5fbef3d068a
Bruno <brunobg@gmail.com>
2020-02-21 12:27:32 -0300

    basic design system

--- pages/design-system.vue
+++ pages/design-system.vue
@@ -1,44 +1,48 @@
 <template>
-  <div class="container">
-    <h1>
+  <article class="container">
+    <h1 class="title is-1">
       Laje VR
       <span>Design System</span>
     </h1>
-    <section>
-      <h2>Tipography</h2>
+
+    <section class="section">
+      <h2 class="title is-2">
+        Typography
+      </h2>
       <hr>
-      <h1>
+      <h1 class="title is-1">
         This is Heading 1 appeareance
         <span class="show-mobile-only">in mobile devices</span>
         <span class="show-tablet-only">in tablet devices</span>
       </h1>
-      <h2>
+      <h2 class="title is-2">
         This is Heading 2 appeareance
         <span class="show-mobile-only">in mobile devices</span>
         <span class="show-tablet-only">in tablet devices</span>
       </h2>
-      <h3>
+      <h3 class="title is-3">
         This is Heading 3 appeareance
         <span class="show-mobile-only">in mobile devices</span>
         <span class="show-tablet-only">in tablet devices</span>
       </h3>
-      <h4>
+      <h4 class="title is-4">
         This is Heading 4 appeareance
         <span class="show-mobile-only">in mobile devices</span>
         <span class="show-tablet-only">in tablet devices</span>
       </h4>
       <p>
-        This is a normal paragraph and heres a text example:
+        This is a normal paragraph <a href="#">with a link</a> and here's a text example:
         <br>The Laje de Santos Marine State Park (Portuguese: Parque Estadual Marinho da Laje de Santos) is a state park in the state of São Paulo, Brazil.
         <br>It protects a marine area off the coast of the state, and was the first such park to be created by São Paulo. It includes a rocky islet, tidal reefs and surrounding waters, with various areas suitable for diving, including a boat that was deliberately wrecked to form an artificial reef. The combination of warm surface waters and cold deeper waters supports a high level of biodiversity.
       </p>
     </section>
-    <section>
+    <section class="section">
       <h2>Colors</h2>
       <hr>
     </section>
-    <section>
+    <section class="section">
       <h2>Elements</h2>
     </section>
-  </div>
+    </div>
+  </article>
 </template>
