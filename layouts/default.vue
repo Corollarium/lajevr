76abf1fca869d79ead8b3ac2c3df61dec5b9de55
Bruno <brunobg@gmail.com>
2020-02-27 18:27:20 -0300

    limpeza

--- layouts/default.vue
+++ layouts/default.vue
@@ -19,31 +19,5 @@ export default {
 </script>
 
 <style lang="less">
-html {
-  word-spacing: 1px;
-  -ms-text-size-adjust: 100%;
-  -webkit-text-size-adjust: 100%;
-  -moz-osx-font-smoothing: grayscale;
-  -webkit-font-smoothing: antialiased;
-  box-sizing: border-box;
-}
-
-html, body {
-  margin:0;
-  padding:0;
-  height:100%;
-}
-
-*,
-*:before,
-*:after {
-  box-sizing: border-box;
-  margin: 0;
-}
-
-figcaption {
-  font-size: 0.8em;
-  font-style: italic;
-}
 
 </style>
