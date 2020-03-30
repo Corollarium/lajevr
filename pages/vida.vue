ac9bd430ff7120f3e9798c6c69dd20a7e872afa2
Bruno <brunobg@gmail.com>
2020-03-30 14:59:03 -0300

    fix attribution

--- pages/vida.vue
+++ pages/vida.vue
@@ -31,21 +31,10 @@
           </i18n>
         </h1>
         <p class="attribution">
-          Photo
-          <a
-            href="//commons.wikimedia.org/w/index.php?title=User:Afcarv&amp;action=edit&amp;redlink=1"
-            class="new"
-            title="User:Afcarv (page does not exist)"
-            target="_blank"
-          >Anael Ferraz de Carvalho</a>
-          -
-          <span class="int-own-work" lang="en">Own work</span>,
-          <a
-            href="https://creativecommons.org/licenses/by-sa/3.0"
-            title="Creative Commons Attribution-Share Alike 3.0"
-            target="_blank"
-          >CC BY-SA 3.0</a>
-          <a target="_blank" href="https://commons.wikimedia.org/w/index.php?curid=40439744">Link</a>
+          <i18n>
+            Foto
+          </i18n>
+          <a target="_blank" href="https://www.instagram.com/nautherandres/">Nauther Andres</a>
         </p>
       </div>
     </section>
