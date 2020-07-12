b285152c269b2ad20d7227c84e2bcbebaeef7796
Vinícius Rodrigues Piva de Carvalho <vinicius.piva@hotmail.com>
2020-07-12 18:15:19 -0300

    10) review bg code / estilo lat long dificuldade

--- pages/pontos-de-mergulho.vue
+++ pages/pontos-de-mergulho.vue
@@ -48,7 +48,9 @@
                   {{ selectedSiteData.dificuldade }}
                 </p>
                 <p class="dive-coordinates">
-                  Latitude: {{ selectedSiteData.lat }} Longitude: {{ selectedSiteData.long }}
+                  <font-awesome-icon icon="arrows-alt-v" />
+                  Latitude: {{ selectedSiteData.lat }} <br>
+                  <font-awesome-icon icon="arrows-alt-h" /> Longitude: {{ selectedSiteData.long }}
                 </p>
                 <p>
                   <span class="dive-description">{{ selectedSiteData.description }}</span>
@@ -133,83 +135,107 @@ export default {
       isSafari: false,
       isIos: false,
       diveSites: [
-        { name: 'Portinho',
+        {
+          name: 'Portinho',
           lat: -24.318083,
           long: -46.183444,
-          description: 'Baixo grau de dificuldade.',
-          dificuldade: 'A face norte é o local onde ocorre a maioria dos mergulhos, com profundidade de até 22m, mais abrigado das correntes e de fácil orientação.'
+          dificuldade: '⭑✩✩ Baixo grau de dificuldade.',
+          description:
+            'A face norte é o local onde ocorre a maioria dos mergulhos, com profundidade de até 22m, mais abrigado das correntes e de fácil orientação.'
         },
-        { name: 'Moréia',
+        {
+          name: 'Moréia',
           lat: -24.317056,
           long: -46.182083,
-          description: 'Baixo grau de dificuldade.',
-          dificuldade: 'Naufrágio do Moréia, um pesqueiro de ferro com 15m de comprimento. Na face norte, próximo à ponta leste, com estrutura em estado instável, desaconselhando penetração, profundidade máxima de 22m.'
+          dificuldade: '⭑✩✩ Baixo grau de dificuldade.',
+          description:
+            'Naufrágio do Moréia, um pesqueiro de ferro com 15m de comprimento. Na face norte, próximo à ponta leste, com estrutura em estado instável, desaconselhando penetração, profundidade máxima de 22m.'
         },
-        { name: 'Piscinas',
+        {
+          name: 'Piscinas',
           lat: -24.321667,
           long: -46.184306,
-          description: 'Médio grau de dificuldade.',
-          dificuldade: 'Na ponta oeste/sudoeste da Laje, ambiente com profundidade que varia de 10 a 35m e requer boa noção de orientação subaquática.'
+          dificuldade: '⭑⭑✩ Médio grau de dificuldade.',
+          description:
+            'Na ponta oeste/sudoeste da Laje, ambiente com profundidade que varia de 10 a 35m e requer boa noção de orientação subaquática.'
         },
-        { name: 'Boca da Baleia',
+        {
+          name: 'Boca da Baleia',
           lat: -24.316778,
           long: -46.179083,
-          description: 'Fenda voltada para leste, com cerca de 50m de extensão e profundidade média de 15m. Requer excelente condição de mar e direção de ondulação adequada para que se possa adentrar.',
-          dificuldade: 'Alto grau de dificuldade'
+          dificuldade: '⭑⭑⭑ Alto grau de dificuldade',
+          description:
+            'Fenda voltada para leste, com cerca de 50m de extensão e profundidade média de 15m. Requer excelente condição de mar e direção de ondulação adequada para que se possa adentrar.'
         },
-        { name: 'Paredão Face Sul',
+        {
+          name: 'Paredão Face Sul',
           lat: -24.320333,
           long: -46.180194,
-          description: 'Médio grau de dificuldade.',
-          dificuldade: 'Encosta rochosa íngreme que desce verticalmente até 42m de profundidade. Mergulhos feitos em "drifting" a favor da corrente. Formação com inclinação negativa entre 12 e 27m de profundidade do centro para leste.'
+          dificuldade: '⭑⭑✩ Médio grau de dificuldade.',
+          description:
+            'Encosta rochosa íngreme que desce verticalmente até 42m de profundidade. Mergulhos feitos em "drifting" a favor da corrente. Formação com inclinação negativa entre 12 e 27m de profundidade do centro para leste.'
         },
-        { name: 'Parcel das Âncoras',
+        {
+          name: 'Parcel das Âncoras',
           lat: -24.321833,
           long: -46.184972,
-          description: 'Alto grau de dificuldade.',
-          dificuldade: 'Fundo rochoso que se destaca da Laje em direção ao continente, apresenta estrutura complexa, exigindo boa orientação subaquática. Profundidade entre 18 e 42m. Presença de muitas âncoras de pesqueiros que ficaram presas ao fundo rochoso. Sujeito a correntes.'
+          dificuldade: '⭑⭑⭑ Alto grau de dificuldade.',
+          description:
+            'Fundo rochoso que se destaca da Laje em direção ao continente, apresenta estrutura complexa, exigindo boa orientação subaquática. Profundidade entre 18 e 42m. Presença de muitas âncoras de pesqueiros que ficaram presas ao fundo rochoso. Sujeito a correntes.'
         },
-        { name: 'Parcel do Sul',
+        {
+          name: 'Parcel do Sul',
           lat: -24.326917,
           long: -46.183722,
-          description: 'Médio grau de dificuldade.',
-          dificuldade: 'Formação submersa a cerca de 400m a sudoeste da Laje, inicia aos 8m e segue até os 42m. Requer mar em boas condições, mas a formação permite orientação e deslocamento mais simples.'
+          dificuldade: '⭑⭑✩ Médio grau de dificuldade.',
+          description:
+            'Formação submersa a cerca de 400m a sudoeste da Laje, inicia aos 8m e segue até os 42m. Requer mar em boas condições, mas a formação permite orientação e deslocamento mais simples.'
         },
-        { name: 'Calhaus Face Norte',
+        {
+          name: 'Calhaus Face Norte',
           lat: -24.327167,
           long: -46.161667,
-          description: 'Baixo a médio grau de dificuldade.',
-          dificuldade: 'Paredão levemente acidentado, com características de navegação subaquática semelhantes ao portinho da Laje, com profundidades que podem variar de 8 a 25m, passando a até 35m se houver afastamento das rochas na direção norte (sentido Laje).'
+          dificuldade: '⭑✩✩ Baixo a ⭑⭑✩ médio grau de dificuldade.',
+          description:
+            'Paredão levemente acidentado, com características de navegação subaquática semelhantes ao portinho da Laje, com profundidades que podem variar de 8 a 25m, passando a até 35m se houver afastamento das rochas na direção norte (sentido Laje).'
         },
-        { name: 'Calhaus Face Sul',
+        {
+          name: 'Calhaus Face Sul',
           lat: -24.32825,
           long: -46.159278,
-          description: 'Paredão levemente acidentado com incidência de correntes e profundidades que podem variar de 8 a 40m.',
-          dificuldade: 'Alto grau de dificuldade.'
+          dificuldade: '⭑⭑✩ Alto grau de dificuldade.',
+          description:
+            'Paredão levemente acidentado com incidência de correntes e profundidades que podem variar de 8 a 40m.'
         },
-        { name: 'Calhaus Túnel',
+        {
+          name: 'Calhaus Túnel',
           lat: -24.328,
           long: -46.160722,
-          description: 'Passagem em forma de ”U” e um arco central emerso, com grande apelo visual e profundidade máxima de 18m, porém exige bom equilíbrio hidrostático por parte do mergulhador. Sujeito a boas condições de mar.',
-          dificuldade: 'Baixo grau de dificuldade.'
+          dificuldade: '⭑✩✩ Baixo grau de dificuldade.',
+          description:
+            'Passagem em forma de ”U” e um arco central emerso, com grande apelo visual e profundidade máxima de 18m, porém exige bom equilíbrio hidrostático por parte do mergulhador. Sujeito a boas condições de mar.'
         },
-        { name: 'Parcel Novo',
+        {
+          name: 'Parcel Novo',
           lat: -24.345472,
           long: -46.173972,
-          description: 'Alto grau de dificuldade.',
-          dificuldade: 'Formação submersa localizada cerca de 1,5 milhas náuticas ao sul da Laje que inicia aos 26m de profundidade e chega aos 45m. Exige mar em excelentes condições e preparo adequado dos mergulhadores.'
+          dificuldade: '⭑⭑⭑ Alto grau de dificuldade.',
+          description:
+            'Formação submersa localizada cerca de 1,5 milhas náuticas ao sul da Laje que inicia aos 26m de profundidade e chega aos 45m. Exige mar em excelentes condições e preparo adequado dos mergulhadores.'
         },
-        { name: 'Parcel do Brilhante',
+        {
+          name: 'Parcel do Brilhante',
           lat: -24.28888889,
           long: -46.16388889,
-          description: '-',
-          dificuldade: 'Não há atividade turística. Área de preservação.'
+          dificuldade: '-',
+          description: 'Não há atividade turística. Área de preservação.'
         },
-        { name: 'Laje do Bandolim',
+        {
+          name: 'Laje do Bandolim',
           lat: -24.27666667,
           long: -46.18194444,
-          description: '-',
-          dificuldade: 'Não há atividade turística. Área de preservação.'
+          dificuldade: '-',
+          description: 'Não há atividade turística. Área de preservação.'
         }
       ]
     };
@@ -232,7 +258,11 @@ export default {
       const r = name ? new RegExp(replaceSpecialChars(name), 'i') : null;
       return this.gallery.filter((i) => {
         // texto
-        if (r && i.description && replaceSpecialChars(i.description).search(r) !== -1) {
+        if (
+          r &&
+          i.description &&
+          replaceSpecialChars(i.description).search(r) !== -1
+        ) {
           return true;
         }
         if (r && i.name && replaceSpecialChars(i.name).search(r) !== -1) {
@@ -252,7 +282,9 @@ export default {
 
   mounted () {
     this.head.title = this.$gettext('Projeto Laje de Santos Virtual');
-    this.head.description = this.$gettext('Sobre o projeto Laje de Santos Virtual');
+    this.head.description = this.$gettext(
+      'Sobre o projeto Laje de Santos Virtual'
+    );
     const browser = Bowser.getParser(window.navigator.userAgent);
     const osName = browser.getOSName(true);
     this.isIos = osName === 'ios';
