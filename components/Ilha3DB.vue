a11efd43bdb0aeacc9f1924f47e1f687c97e1228
Bruno <brunobg@gmail.com>
2021-11-12 12:32:54 -0300

    ilha, babylon version

---
+++ components/Ilha3DB.vue
@@ -0,0 +1,260 @@
+<template>
+  <canvas id="ilha-container" />
+</template>
+
+<script>
+/* eslint-disable */
+import * as BABYLON from 'babylonjs';
+import 'babylonjs-loaders';
+import { WaterMaterial } from 'babylonjs-materials';
+/* eslint-enable */
+
+export default {
+  props: {
+    diveSites: {
+      type: Array,
+      default () { return []; }
+    }
+  },
+
+  data () {
+    return {
+      engine: null,
+      scene: null,
+      camera: null,
+      container: null
+    };
+  },
+
+  mounted () {
+    const SCENE_SIZE = 7000;
+
+    function latlongToPixel (lat, long) {
+      const latmin = -24.26333333; const latmax = -24.35333333; const lattotal = (latmin - latmax);
+      const longmin = -46.15000; const longmax = -46.2000; const longtotal = (longmin - longmax);
+
+      return [
+        7000 * (latmin - lat) / lattotal - 4500,
+        7000 * (longmin - long) / longtotal - 4500
+      ];
+    }
+
+    this.container = document.getElementById('ilha-container');
+
+    this.engine = new BABYLON.Engine(this.container, true); // Generate the BABYLON 3D engine
+    this.engine.loadingUIText = 'Mergulho na Laje de Santos';
+
+    this.scene = new BABYLON.Scene(this.engine);
+    this.scene.clearColor = BABYLON.Color3.FromHexString('0x46BCEC');
+    // this.camera = new THREE.PerspectiveCamera(75, this.container.clientWidth / this.container.clientHeight, 1, 20000);
+    // Add a camera to the scene and attach it to the canvas
+    this.camera = new BABYLON.ArcRotateCamera(
+      'Camera',
+      0.0,
+      30.0 / Math.PI,
+      500,
+      new BABYLON.Vector3(0, 0, 0), // target
+      this.scene
+    );
+    this.camera.position = new BABYLON.Vector3(0, 1000, 100);
+    this.camera.minZ = 1.0;
+    this.camera.maxZ = 2 * SCENE_SIZE;
+    this.camera.useFramingBehavior = true;
+    this.camera.applyGravity = false;
+    this.camera.upperBetaLimit = Math.PI / 2.0 - Math.PI / 20.0;
+    this.camera.upperRadiusLimit = SCENE_SIZE * 0.5;
+    this.camera.inertialRadiusOffset = 5;
+    this.camera.attachControl(this.container, false);
+    this.camera.inputs.attached.mousewheel.wheelDeltaPercentage = 0.05;
+    // lights
+
+    // Add lights to the scene
+    this.light1 = new BABYLON.HemisphericLight('light1', new BABYLON.Vector3(1, 1, 0), this.scene);
+    this.light2 = new BABYLON.DirectionalLight('light2', new BABYLON.Vector3(1, -1, 0), this.scene);
+
+    // loading manager
+    const loader = new BABYLON.AssetsManager(this.scene);
+
+    const siteMaterial = new BABYLON.StandardMaterial('site', this.scene);
+    siteMaterial.diffuseColor = BABYLON.Color3.FromHexString('#0077FF');
+    const siteSelectedMaterial = new BABYLON.StandardMaterial('siteSelected', this.scene);
+    siteSelectedMaterial.diffuseColor = BABYLON.Color3.FromHexString('#FF0000');
+
+    // const siteMeshes = {};
+    const spriteManagerPlayer = new BABYLON.SpriteManager('playerManager', '/models/player.png', this.diveSites.length, 64, this.scene);
+    let buoy = null;
+    loader.addMeshTask('flag', null, this.$router.options.base + 'models/flagLoop.glb').onSuccess = (task) => {
+      console.log(task.loadedMeshes);
+      buoy = BABYLON.Mesh.MergeMeshes(
+        [
+          task.loadedMeshes[1],
+          task.loadedMeshes[2],
+          task.loadedMeshes[3]
+        ],
+        true,
+        true,
+        undefined,
+        false,
+        true
+      );
+      let i = 0;
+      for (const site of this.diveSites) {
+        const pos = latlongToPixel(site.lat, site.long);
+        const matrix = BABYLON.Matrix.Scaling(15.0, 15.0, 15.0);
+        matrix.addTranslationFromFloats(pos[0], 20, pos[1]);
+        buoy.thinInstanceAdd(matrix);
+        // mesh.userData = site;
+        // siteMeshes[site.name] = mesh;
+
+        const player = new BABYLON.Sprite('player' + i, spriteManagerPlayer);
+        player.cellIndex = i;
+        i++;
+        player.position = new BABYLON.Vector3(pos[0], 150, pos[1]);
+        player.width = 100;
+        player.height = 100;
+      }
+    };
+
+    // collada
+    const v = loader.addMeshTask('map', null, this.$router.options.base + 'PEMLSdae/', 'PEMLS_skt.dae');
+    v.onSuccess = (task) => {
+      console.log('lfl');
+    //   land = collada.scene;
+    //   land.position.set(220, 0, -100);
+    //   const degrees = 80;
+    //   land.rotateZ((degrees / 180.0) * Math.PI);
+    };
+    v.onError = (x) => {
+      console.log(x);
+    };
+
+    // Skybox
+    const skybox = BABYLON.Mesh.CreateBox('skyBox', SCENE_SIZE * 1.2, this.scene);
+    const skyboxMaterial = new BABYLON.StandardMaterial('skyBox', this.scene);
+    skyboxMaterial.backFaceCulling = false;
+    skyboxMaterial.reflectionTexture = new BABYLON.CubeTexture('https://playground.babylonjs.com/textures/TropicalSunnyDay', this.scene);
+    skyboxMaterial.reflectionTexture.coordinatesMode = BABYLON.Texture.SKYBOX_MODE;
+    skyboxMaterial.diffuseColor = new BABYLON.Color3(0, 0, 0);
+    skyboxMaterial.specularColor = new BABYLON.Color3(0, 0, 0);
+    skyboxMaterial.disableLighting = true;
+    skybox.material = skyboxMaterial;
+
+    const waterMesh = BABYLON.MeshBuilder.CreateGround('waterMesh', { width: SCENE_SIZE * 1.1, height: SCENE_SIZE * 1.1, subdivisions: 16 }, this.scene);
+    const water = new WaterMaterial('water', this.scene, new BABYLON.Vector2(512, 512));
+    water.backFaceCulling = true;
+    water.bumpTexture = new BABYLON.Texture('https://playground.babylonjs.com/textures/waterbump.png', this.scene);
+    water.windForce = 0;
+    water.waveHeight = 3.7;
+    water.bumpHeight = 0.7;
+    water.windDirection = new BABYLON.Vector2(1, 1);
+    water.waterColor = new BABYLON.Color3(0, 0, 221 / 255);
+    water.colorBlendFactor = 0.0;
+    water.addToRenderList(skybox);
+    waterMesh.material = water;
+
+    // const gsize = 8192;
+    // const res = 1024;
+    // const gres = res / 2;
+    // const origx = -gsize / 2;
+    // const origz = -gsize / 2;
+    // const ocean = new Ocean(this.renderer, this.camera, scene,
+    //   {
+    //     USE_HALF_FLOAT: false,
+    //     INITIAL_SIZE: 2048.0,
+    //     INITIAL_WIND: [ 4.0, 4.0 ],
+    //     INITIAL_CHOPPINESS: 0.3,
+    //     CLEAR_COLOR: [ 1.0, 1.0, 1.0, 0.0 ],
+    //     GEOMETRY_ORIGIN: [ origx, origz ],
+    //     SUN_DIRECTION: [ -1.0, 1.0, 1.0 ],
+    //     OCEAN_COLOR: new THREE.Vector3(0.004, 0.016, 0.047),
+    //     SKY_COLOR: new THREE.Vector3(3.2, 9.6, 12.8),
+    //     EXPOSURE: 0.35,
+    //     GEOMETRY_RESOLUTION: gres,
+    //     GEOMETRY_SIZE: gsize,
+    //     RESOLUTION: res
+    //   });
+
+    // ocean.materialOcean.uniforms.u_projectionMatrix = { value: this.camera.projectionMatrix };
+    // ocean.materialOcean.uniforms.u_viewMatrix = { value: this.camera.matrixWorldInverse };
+    // ocean.materialOcean.uniforms.u_cameraPosition = { value: this.camera.position };
+    // ocean.oceanMesh.scale.set(2, 2, 2);
+    // scene.add(ocean.oceanMesh);
+
+    this.scene.onPointerDown = function (evt, pickResult) {
+      if (buoy && pickResult.hit) {
+        const worldMat = buoy.thinInstanceGetWorldMatrices();
+        const thinInstanceMatrix = worldMat[pickResult.thinInstanceIndex];
+
+        console.log(pickResult.pickedPoint, pickResult.thinInstanceIndex, thinInstanceMatrix);
+
+        // for (const name in siteMeshes) {
+        //   siteMeshes[object.userData.name].material = siteSelectedMaterial;
+        //   this.$emit('pick', object.userData.name);
+        // }
+      } else {
+        // boxSelected.setEnabled(false);
+      }
+    };
+    // const clickEvent = (e) => {
+    //   const rect = this.renderer.domElement.getBoundingClientRect();
+    //   mouse.x = ((event.clientX - rect.left) / (rect.right - rect.left)) * 2 - 1;
+    //   mouse.y = -((event.clientY - rect.top) / (rect.bottom - rect.top)) * 2 + 1;
+
+    //   raycaster.setFromCamera(mouse, this.camera);
+
+    //   const intersects = raycaster.intersectObject(diveSiteGroup, true);
+
+    //   if (intersects.length > 0) {
+    //     const object = intersects[0].object;
+
+    //     for (const name in siteMeshes) {
+    //       siteMeshes[name].material = siteMaterial;
+    //     }
+    //     siteMeshes[object.userData.name].material = siteSelectedMaterial;
+    //     this.$emit('pick', object.userData.name);
+    //   }
+    // };
+
+    this.$on('picker', (selectedIndex) => {
+    //   for (const name in siteMeshes) {
+    //     siteMeshes[name].material = siteMaterial;
+    //   }
+    //   siteMeshes[this.diveSites[selectedIndex].name].material = siteSelectedMaterial;
+    });
+
+    loader.load();
+    this.engine.runRenderLoop(() => {
+      // TODO const deltaTime = this.engine.getDeltaTime() / 1000.0; // in s
+
+      this.scene.render();
+    });
+
+    // // on initialization
+    window.addEventListener('resize', this.onWindowResize, false);
+  },
+
+  beforeDestroy () {
+    window.removeEventListener('resize', this.onWindowResize);
+    this.engine = null;
+  },
+
+  methods: {
+    onWindowResize () {
+      this.engine.resize();
+    }
+  }
+};
+</script>
+
+<style lang="less" scoped>
+#ilha-container {
+  width: 100%;
+  height: 100%;
+
+  #ocean {
+    position: absolute;
+    height: 100%;
+  }
+}
+
+</style>
---
+++ pages/pontos-de-mergulho-b.vue
@@ -0,0 +1,326 @@
+<template>
+  <article class="dive-container">
+    <aside :class="{'dive-aside': true, 'dive-aside-showBottomSheet': showBottomSheet}">
+      <section class="dive-point-info">
+        <div @click="showBottomSheet = !showBottomSheet" class="dive-swipe-show">
+          <span class="indicador-swipe" /></span>
+        </div>
+        <div v-show="selectedSite == -1">
+          <h1 class="dive-point-title">
+            <i18n>
+              Pontos de Mergulho na Laje de Santos
+            </i18n>
+          </h1>
+
+          <p>
+            <i18n>
+              Clique em um ponto de mergulho para ter mais informações.
+            </i18n>
+          </p>
+
+          <p>
+            <i18n>
+              A Laje de Santos é um local popular para a prática do mergulho. Próxima da cidade de São Paulo, a 1h30m de barco da costa e
+              com uma diversidade de espécies que poucos lugares são capazes de igualar, raramente com menos de 10 metros de visibilidade.
+              Selecione um local para ter mais informações.
+            </i18n>
+          </p>
+
+          <p>
+            <i18n>Fontes das informações:</i18n>
+            <a target="_blank" href="https://www.lajeviva.org.br/Lajeviva/parque/pontos-de-mergulho/">Instituto Laje Viva</a>,
+            <a target="_blank" href="http://arquivos.ambiente.sp.gov.br/fundacaoflorestal/2014/04/Plano-Emergencial-de-Uso-P%C3%BAblico_PEMLS_versaoFinal.pdf">Plano Emergencial de Uso Público do PEMLS</a>
+            <a target="_blank" href="https://smastr16.blob.core.windows.net/consema/2018/11/e-laje-de-santos-plano-de-manejo.pdf">Plano de Manejo da Laje de Santos</a>
+          </p>
+        </div>
+        <transition name="fade-in-up">
+          <div
+            v-if="swapping"
+          >
+            <div
+              v-show="selectedSite != -1"
+            >
+              <h2>
+                {{ selectedSiteData.name }}
+              </h2>
+              <div>
+                <p class="dive-dificuldade">
+                  {{ selectedSiteData.dificuldade }}
+                </p>
+                <p class="dive-coordinates">
+                  <font-awesome-icon icon="arrows-alt-v" />
+                  Latitude: {{ selectedSiteData.lat }} <br>
+                  <font-awesome-icon icon="arrows-alt-h" /> Longitude: {{ selectedSiteData.long }}
+                </p>
+                <p>
+                  <span class="dive-description">{{ selectedSiteData.description }}</span>
+                </p>
+              </div>
+              <div class="galeria-pontos">
+                <GalleryCard
+                  v-for="(a, i) in filteredGallery"
+                  :key="i"
+                  v-bind="a"
+                />
+              </div>
+            </div>
+          </div>
+        </transition>
+      </section>
+    </aside>
+
+    <section :class="{'dive-3d': true, 'dive-3d-apple': isApple}">
+      <select
+        v-model="selectedSite"
+        @input="swapping = false; selectSite($event.target.value);"
+        class="dive-point-selector"
+      >
+        <option
+          v-for="(d, i) in diveSites"
+          :key="d.name"
+          :value="i"
+        >
+          {{ d.name }}
+        </option>
+      </select>
+
+      <section v-if="isApple" class="pontos-apple">
+        <img :src="imagePontosAppleDesk" :srcset="imagePontosAppleDesk.srcSet" class="pontos-apple-desktop" alt="Gráfico da Laje com Pontos de Mergulho">
+        <img :src="imagePontosAppleMobile" :srcset="imagePontosAppleMobile.srcSet" class="pontos-apple-mobile" alt="Gráfico da Laje com Pontos de Mergulho">
+      </section>
+
+      <Ilha3D
+        ref="ilha3d"
+        :dive-sites="diveSites"
+        v-on:pick="pick"
+        v-else
+      />
+    </section>
+  </article>
+</template>
+
+<!-- PRECISO QUE ISSO SO FUNCIONE NESSA PÁGINA...
+ <style lang="less">
+  #__layout {
+    height: 100vh;
+    overflow: hidden;
+  }
+</style> -->
+
+<script>
+import Bowser from 'bowser';
+import page from './page.vue';
+import Ilha3D from '~/components/Ilha3DB.vue';
+import GalleryCard from '~/components/sections/GalleryCard.vue';
+import galleryData from '~/components/galleryData.js';
+
+const imagePontosAppleDesk = require('~/assets/images/graficos/pontos-ios-desktop.jpg?resize');
+const imagePontosAppleMobile = require('~/assets/images/graficos/pontos-ios-mobile.jpg?resize');
+
+export default {
+  components: {
+    GalleryCard,
+    Ilha3D
+  },
+
+  extends: page,
+
+  data () {
+    return {
+      imagePontosAppleDesk,
+      imagePontosAppleMobile,
+      gallery: galleryData,
+      selectedSite: -1,
+      swapping: false,
+      showBottomSheet: false,
+      isApple: false,
+      isSafari: false,
+      isIos: false,
+      diveSites: [
+        {
+          name: 'Portinho',
+          lat: -24.318083,
+          long: -46.183444,
+          dificuldade: '⭑✩ Baixo grau de dificuldade.',
+          description:
+            'A face norte é o local onde ocorre a maioria dos mergulhos, com profundidade de até 22m, mais abrigado das correntes e de fácil orientação.'
+        },
+        {
+          name: 'Naufrágio Moréia',
+          lat: -24.317056,
+          long: -46.182083,
+          dificuldade: '⭑✩ Baixo grau de dificuldade.',
+          description:
+            'Naufrágio do Moréia, um pesqueiro de ferro com 15m de comprimento. Na face norte, próximo à ponta leste, com estrutura em estado instável, desaconselhando penetração, profundidade máxima de 22m.'
+        },
+        {
+          name: 'Piscinas',
+          lat: -24.321667,
+          long: -46.184306,
+          dificuldade: '⭑⭑✩✩ Médio grau de dificuldade.',
+          description:
+            'Na ponta oeste/sudoeste da Laje, ambiente com profundidade que varia de 10 a 35m e requer boa noção de orientação subaquática.'
+        },
+        {
+          name: 'Boca da Baleia',
+          lat: -24.316778,
+          long: -46.179083,
+          dificuldade: '✩✩✩ Alto grau de dificuldade',
+          description:
+            'Fenda voltada para leste, com cerca de 50m de extensão e profundidade média de 15m. Requer excelente condição de mar e direção de ondulação adequada para que se possa adentrar.'
+        },
+        {
+          name: 'Paredão Face Sul',
+          lat: -24.320333,
+          long: -46.180194,
+          dificuldade: '⭑⭑✩✩ Médio grau de dificuldade.',
+          description:
+            'Encosta rochosa íngreme que desce verticalmente até 42m de profundidade. Mergulhos feitos em "drifting" a favor da corrente. Formação com inclinação negativa entre 12 e 27m de profundidade do centro para leste.'
+        },
+        {
+          name: 'Parcel das Âncoras',
+          lat: -24.321833,
+          long: -46.184972,
+          dificuldade: '✩✩✩ Alto grau de dificuldade.',
+          description:
+            'Fundo rochoso que se destaca da Laje em direção ao continente, apresenta estrutura complexa, exigindo boa orientação subaquática. Profundidade entre 18 e 42m. Presença de muitas âncoras de pesqueiros que ficaram presas ao fundo rochoso. Sujeito a correntes.'
+        },
+        {
+          name: 'Parcel do Sul',
+          lat: -24.326917,
+          long: -46.183722,
+          dificuldade: '⭑⭑✩✩ Médio grau de dificuldade.',
+          description:
+            'Formação submersa a cerca de 400m a sudoeste da Laje, inicia aos 8m e segue até os 42m. Requer mar em boas condições, mas a formação permite orientação e deslocamento mais simples.'
+        },
+        {
+          name: 'Calhaus Face Norte',
+          lat: -24.327167,
+          long: -46.161667,
+          dificuldade: '⭑✩ Baixo a ⭑⭑✩✩ Médio grau de dificuldade.',
+          description:
+            'Paredão levemente acidentado, com características de navegação subaquática semelhantes ao portinho da Laje, com profundidades que podem variar de 8 a 25m, passando a até 35m se houver afastamento das rochas na direção norte (sentido Laje).'
+        },
+        {
+          name: 'Calhaus Face Sul',
+          lat: -24.32825,
+          long: -46.159278,
+          dificuldade: '⭑⭑✩✩✩ Alto grau de dificuldade.',
+          description:
+            'Paredão levemente acidentado com incidência de correntes e profundidades que podem variar de 8 a 40m.'
+        },
+        {
+          name: 'Calhaus Túnel',
+          lat: -24.328,
+          long: -46.160722,
+          dificuldade: '⭑✩ Baixo grau de dificuldade.',
+          description:
+            'Passagem em forma de ”U” e um arco central emerso, com grande apelo visual e profundidade máxima de 18m, porém exige bom equilíbrio hidrostático por parte do mergulhador. Sujeito a boas condições de mar.'
+        },
+        {
+          name: 'Parcel Novo',
+          lat: -24.345472,
+          long: -46.173972,
+          dificuldade: '⭑⭑⭑✩✩✩ Alto grau de dificuldade.',
+          description:
+            'Formação submersa localizada cerca de 1,5 milhas náuticas ao sul da Laje que inicia aos 26m de profundidade e chega aos 45m. Exige mar em excelentes condições e preparo adequado dos mergulhadores.'
+        },
+        {
+          name: 'Parcel do Brilhante',
+          lat: -24.28888889,
+          long: -46.16388889,
+          dificuldade: '-',
+          description: 'Não há atividade turística. Área de preservação.'
+        },
+        {
+          name: 'Laje do Bandolim',
+          lat: -24.27666667,
+          long: -46.18194444,
+          dificuldade: '-',
+          description: 'Não há atividade turística. Área de preservação.'
+        }
+      ]
+    };
+  },
+
+  computed: {
+    filteredGallery () {
+      if (this.selectedSite === -1) {
+        return [];
+      }
+      function replaceSpecialChars (str) {
+        str = str.replace(/[ÀÁÂÃÄÅ]/, 'A');
+        str = str.replace(/[àáâãäå]/, 'a');
+        str = str.replace(/[ÈÉÊË]/, 'E');
+        str = str.replace(/[Ç]/, 'C');
+        str = str.replace(/[ç]/, 'c');
+        return str.replace(/[^a-z0-9]/gi, '');
+      }
+      const name = this.diveSites[this.selectedSite].name;
+      const r = name ? new RegExp(replaceSpecialChars(name), 'i') : null;
+      return this.gallery.filter((i) => {
+        // texto
+        if (
+          r &&
+          i.description &&
+          replaceSpecialChars(i.description).search(r) !== -1
+        ) {
+          return true;
+        }
+        if (r && i.name && replaceSpecialChars(i.name).search(r) !== -1) {
+          return true;
+        }
+        return false;
+      }, this);
+    },
+
+    selectedSiteData () {
+      if (this.selectedSite === -1) {
+        return {};
+      }
+      return this.diveSites[this.selectedSite];
+    }
+  },
+
+  mounted () {
+    this.head.title = this.$gettext('Projeto Laje de Santos Virtual');
+    this.head.description = this.$gettext(
+      'Sobre o projeto Laje de Santos Virtual'
+    );
+    const browser = Bowser.getParser(window.navigator.userAgent);
+    const osName = browser.getOSName(true);
+    this.isIos = osName === 'ios';
+    this.isSafari = browser.satisfies({
+      // declare browsers per OS
+      macos: {
+        safari: '>10.1'
+      }
+    });
+    this.isApple = this.isIos || this.isSafari;
+  },
+
+  methods: {
+    // callback for select
+    selectSite (diveSiteIndex) {
+      if (this.$refs.ilha3d) {
+        this.$refs.ilha3d.$emit('picker', diveSiteIndex);
+      }
+      this.pick(this.diveSites[diveSiteIndex]);
+    },
+
+    // callback for select/ 3d pick, apply transition properly
+    pick (diveSite) {
+      this.swapping = !this.swapping;
+      for (let i = 0; i < this.diveSites.length; i++) {
+        if (this.diveSites[i].name === diveSite) {
+          this.$nextTick(() => {
+            this.selectedSite = i;
+            this.swapping = true;
+          });
+          break;
+        }
+      }
+    }
+  }
+};
+</script>
