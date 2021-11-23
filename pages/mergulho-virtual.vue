5af6339a26f88e7b839d432f3d569693056620d8
Bruno <brunobg@gmail.com>
2021-11-23 11:44:28 -0300

    gpu check

--- pages/mergulho-virtual.vue
+++ pages/mergulho-virtual.vue
@@ -1,12 +1,28 @@
 <template>
   <article>
     <section>
+      <h1><i18n>Mergulho virtual</i18n></h1>
+      <p>
+        <i18n>
+          Experimente um mergulho virtual na Laje de Santos e veja o naufrágio Moreia e a vida.
+          É preciso que seu aparelho tenha suporte a 3D para executar este ambiente com bom
+          desempenho. Expectativa de desempenho:
+        </i18n>
+        <span v-show="!gpuTierLoaded"><i18n>Testando desempenho...</i18n></span>
+        <span v-show="gpuTierLoaded" style="font-weight: bold">
+          <span v-if="gpuTier.tier === 0"><i18n>Muito baixo desempenho, muito devagar ou não funcionará</i18n></span>
+          <span v-if="gpuTier.tier === 1"><i18n>Baixo desempenho, devagar</i18n></span>
+          <span v-if="gpuTier.tier === 2"><i18n>Médio desempenho</i18n></span>
+          <span v-if="gpuTier.tier === 3"><i18n>Ótimo desempenho</i18n></span>
+        </span>
+      </p>
       <Underwater />
     </section>
   </article>
 </template>
 
 <script>
+import { getGPUTier } from 'detect-gpu';
 import page from './page.vue';
 import Underwater from '~/components/Underwaterb.vue';
 
@@ -20,13 +36,29 @@ export default {
 
   data () {
     return {
+      gpuTierLoaded: false,
+      gpuTier: {
+        'tier': -1,
+        'isMobile': false,
+        'type': '',
+        'fps': 0,
+        'gpu': ''
+      }
     };
   },
 
   mounted () {
     this.head.title = this.$gettext('Mergulho virtual');
     this.head.description = this.$gettext('Mergulho em realidade virtual na Laje de Santos');
-  }
+    this.getGpuTier();
+  },
 
+  methods: {
+    async getGpuTier () {
+      const t = await getGPUTier();
+      this.$set(this, 'gpuTier', t);
+      this.gpuTierLoaded = true;
+    }
+  }
 };
 </script>
