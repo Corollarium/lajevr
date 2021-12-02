c996d0de4298b16f4bc4dac1d73b06439980561c
Bruno <brunobg@gmail.com>
2021-12-02 19:52:46 -0300

    fixes mobile

--- pages/mergulho-virtual.vue
+++ pages/mergulho-virtual.vue
@@ -11,9 +11,9 @@
         <span v-show="!gpuTierLoaded"><i18n>Testando desempenho...</i18n></span>
         <span v-show="gpuTierLoaded" style="font-weight: bold">
           <span v-if="gpuTier.tier === 0"><i18n>Muito baixo desempenho, muito devagar ou não funcionará</i18n></span>
-          <span v-if="gpuTier.tier === 1"><i18n>Baixo desempenho, devagar</i18n></span>
-          <span v-if="gpuTier.tier === 2"><i18n>Médio desempenho</i18n></span>
-          <span v-if="gpuTier.tier === 3"><i18n>Ótimo desempenho</i18n></span>
+          <span v-else-if="gpuTier.tier === 1"><i18n>Baixo desempenho, devagar</i18n></span>
+          <span v-else-if="gpuTier.tier === 2"><i18n>Médio desempenho</i18n></span>
+          <span v-else-if="gpuTier.tier === 3"><i18n>Ótimo desempenho</i18n></span>
         </span>
       </p>
       <Underwater />
--- plugins/aos.js
+++ plugins/aos.js
@@ -4,7 +4,10 @@ import 'aos/dist/aos.css';
 
 export default ({ app }) => {
   app.AOS = new AOS.init({ // eslint-disable-line new-cap
-    disable: 'phone',
+    disable () {
+      const maxWidth = 800;
+      return window.innerWidth < maxWidth;
+    },
     duration: 1000,
     delay: 250
   });
