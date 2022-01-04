6e46423bd686c1c3e5edf6017a872d9ed138e817
Rodrigo de Godoy Domingues <rodrigod@hiperlogic.com.br>
2022-01-04 10:29:00 -0300

    Codificação do componente de extração da serialização da Textura de Animação de Vértices para JSon. O componente verifica se o JSOn existe. Se sim, ele usa, se não, extrai e baixa. Arquivo deve ser colocado na pasta static/bakedAnim A animação não está sendo executada nessa página, mas a extração e execução foi verificada no componente Boids

---
+++ pages/AnimBakeExtractor.vue
@@ -0,0 +1,104 @@
+<template>
+  <article class="pagina-vida">
+    <article id="turtles" class="article-index">
+      <section class="section-container container">
+        <h2 class="title-specimen">
+          Animation Baking Extractor
+        </h2>
+        <div>
+          <p>
+            <i18n>Extrai o baking da Textura de Animação de Vértices para um arquivo JSOn, caso ele não exista.</i18n>
+            <br>
+            <i18n>Ajuste o modelo, a quantidade de quadros e um delta de quadros por segundo.</i18n>
+            <br>
+            <i18n>Se o arquivo não baixar, significa que ele já existe no servidor.</i18n>
+            <br>
+            <i18n>Improvisações: Animação não rodando nessa página. Aplicar deslocamento de animação, para animação composta.</i18n>
+          </p>
+        </div>
+
+        <div class="model-container" style="min-height: 70vh;" bp="12 6@md">
+          <!-- <babylon
+            id="model-babylon"
+            :model="'./models/anjoReal.glb'"
+            :model="'./models/tartaruga/tartaruga.glb'"
+          /> -->
+          <EBA
+            id="x1111"
+            v-if="engine"
+            :model="'./models/tartaruga/tartaruga.glb'"
+            :attribution="'Modelo de tartaruga'"
+            :babylonEngine="engine"
+            :animationFrames="60"
+            :fpsDelta="50"
+          />
+          <EBA
+            id="x22222"
+            v-if="engine"
+            :model="'./models/frade.glb'"
+            :attribution="'Modelo de tartaruga'"
+            :babylonEngine="engine"
+            :animationFrames="90"
+            :fpsDelta="50"
+          />
+        </div>
+      </section>
+    </article>
+  </article>
+</template>
+
+<script>
+import { Engine } from 'babylonjs';
+import page from './page.vue';
+import EBA from '~/components/EBA.vue';
+
+export default {
+  components: {
+    EBA
+  },
+
+  extends: page,
+
+  data () {
+    return {
+      rellax: null,
+      engine: null
+    };
+  },
+
+  mounted () {
+    const canvas = document.createElement('canvas');
+    this.engine = new Engine(canvas, true);
+    this.engine.loadingUIText = 'Mergulho na Laje de Santos';
+    this.engine.sceneList = {};
+    this.engine.runRenderLoop(() => {
+      for (const i in this.engine.sceneList) {
+        const s = this.engine.sceneList[i];
+        if (this.engine.activeView && this.engine.activeView.target === s.container) {
+          s.renderScene();
+        }
+      }
+    });
+
+    this.head.title = this.$gettext('Vida na Laje de Santos');
+    this.head.description = this.$gettext('Sobre seres vivos na Laje de Santos');
+    // BabylonViewer.InitTags('babylon');
+    // const domElement = document.getElementById('model-container');
+
+    // this.viewer = new BabylonViewer.DefaultViewer(domElement, {
+    //   scene: {
+    //     debug: true
+    //   },
+    //   camera: {
+    //     behaviors: {
+    //       autoRotate: 0
+    //     }
+    //   },
+    //   model: {
+    //     url: 'https://playground.babylonjs.com/scenes/Rabbit.babylon'
+    //   }
+    // });
+  }
+
+};
+</script>
